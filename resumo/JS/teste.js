2807ebf67426bc780e32f05e277d161c95755db8
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-02-28 11:49:53 -0300

    ok

---
+++ resumo/JS/teste.js
@@ -0,0 +1,57 @@
+
+/*
+function cor(){
+   var foto = window.document.createElement('img')
+   foto.setAttribute('id','img')
+   foto.setAttribute('src','https://th.bing.com/th/id/OIF.1Px783lmAgdOuhAuyu8Buw?w=217&h=200&c=7&r=0&o=5&dpr=1.3&pid=1.7')
+
+   var res = window.document.getElementsByTagName('tfoot')[0]
+
+   res.appendChild(foto)
+
+}
+ */
+
+/*var x = Number()
+var y = Number.parseInt()
+var z = Number.parseFloat()
+
+var A = String(window.document)
+var B = toString()
+
+var C = `${A.length}`
+var D = `${B.toUpperCase()}`
+var E = `${A.toLowerCase()}`
+ */
+
+/*var N1 = 1541.5
+
+console.log(`${N1.toFixed(3)}`)
+
+console.log(`${N1.toFixed(2).replace('.',',')}`)
+
+console.log(`${N1.toLocaleString('pt-BR',{style:'currency',currency:'BRL'})}`) */
+
+
+
+/*var x = 10
+var y = '5'
+
+var A = x > y ? `${x} é maior que ${y}` : `${x} não é maior que ${y}`
+
+console.log(`${A}`)
+
+    history        heeady
+window document html 
+    location       body
+
+var A. = window.document.getElementById() //id
+var B = window.document.getElementsByClassName()[0] // class
+var C = window.document.getElementsByName()[] // name
+var D = window.document.getElementsByTagName()[] // tag htmll
+var E = window.document.querySelector() // caminho da tag
+var F = window.document.querySelectorAll()
+
+var bb = 13
+bb. */
+
