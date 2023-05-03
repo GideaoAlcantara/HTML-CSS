f92e51dd0cd81860a7fdb16d9b26e4a49c58617d
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-05-03 17:34:18 -0300

    aula concluida menu

---
+++ EXERCICIOS/modulo004/menuresponsivo/script/menu.js
@@ -0,0 +1,9 @@
+
+function menu(){
+    let item = window.document.getElementById('item')
+    if ( item.style.display == 'block') {
+        item.style.display='none'
+    } else {
+        item.style.display='block' 
+    }
+}
