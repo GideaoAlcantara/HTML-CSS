75feee5928695285d7c4bcd1abee9ea6b3f8ba98
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-05-08 17:03:59 -0300

    aula concluida

--- EXERCICIOS/modulo004/menuresponsivo/script/menu.js
+++ EXERCICIOS/modulo004/menuresponsivo/script/menu.js
@@ -1,9 +1,18 @@
 
+//funcao ao clicar no menu ambuguer faz aparecer as opcoes
 function menu(){
     let item = window.document.getElementById('item')
     if ( item.style.display == 'block') {
         item.style.display='none'
     } else {
         item.style.display='block' 
+    }
+}
+//funcao que faz tirar o erro do menu ambuguer ao cliacr e aumentar a tela
+function mudoutamanho() {
+    if (window.innerWidth >= 770) {
+           item.style.display = 'block'
+    } else {
+        item.style.display='none'
     }
 }
