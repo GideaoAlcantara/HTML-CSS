3bceed7680247acc7f30030f3889af9568a589bf
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-05-29 19:58:19 -0300

    ok

---
+++ revisaohtml/script/menu.js
@@ -0,0 +1,9 @@
+function menu() {
+    let men = window.document.querySelector('ul#menu')
+    
+    if (men.style.display=='block') {
+           men.style.display='none'
+    } else {
+           men.style.display='block'
+    }
+}
