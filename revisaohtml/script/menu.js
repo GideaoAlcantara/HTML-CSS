fa320fda007d86e4d4822317b2523cd38b25199f
Gideao_Alcantara <122929922+GideaoAlcantara@users.noreply.github.com>
2023-09-09 12:08:16 -0300

    um dia concluido

--- revisaohtml/script/menu.js
+++ revisaohtml/script/menu.js
@@ -1,5 +1,5 @@
-function menu() {
-    let men = window.document.querySelector('div#menu')
+function botao() {
+    let men = window.document.querySelector('ul#menu')
     
     if (men.style.display=='block') {
            men.style.display='none'
